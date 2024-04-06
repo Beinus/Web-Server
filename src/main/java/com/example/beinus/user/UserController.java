ee9c9aa3f60478c54727c54248e7b18a1ff2a31d
HyunJong Kim <world2222222222@gmail.com>
2024-04-06 15:07:06 +0900

    FEAT: Implement user api
    Implemented the items below...
    1. User class
    2. UserRepository
    3. UserService
    4. UserController
    
    Modified the itmes below...
    1. Story class
    2. StoryService
    3. StoryController
    
    What's new?
    * Now It's possible to deal with the user api in react.
    * Created the OneToMany relationship between User and Story class.
    * If the userName is updated, all the userName in the user's stories
      will also be updated.

---
+++ src/main/java/com/example/beinus/user/UserController.java
@@ -0,0 +1,39 @@
+package com.example.beinus.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/get-all")
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable String userId) {
+        return service.getUser(userId);
+    }
+
+    @PostMapping("/save")
+    public User saveUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateUser(@PathVariable String userId, @RequestBody User user) {
+        return service.updateUser(user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable String userId) {
+        service.deleteUser(service.getUser(userId));
+    }
+}
